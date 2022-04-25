f32838d0e28c4fcee734972838297c2ad48a4c9c
DraganaTanevska <94012629+DraganaTanevska@users.noreply.github.com>
2022-04-25 20:26:33 +0200

    Delete backend directory

--- backend/src/main/java/mk/ukim/finki/emt/lab2022/library/model/enumerations/Role.java
+++
@@ -1,5 +0,0 @@
-package mk.ukim.finki.emt.lab2022.library.model.enumerations;
-
-public enum Role {
-    ROLE_librarian
-}
--- backend/src/main/java/mk/ukim/finki/emt/lab2022/library/model/exceptions/AuthorNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package mk.ukim.finki.emt.lab2022.library.model.exceptions;
-
-import java.util.function.Supplier;
-
-public class AuthorNotFoundException extends RuntimeException {
-    public AuthorNotFoundException(Long id){
-        super(String.format("Author with id: %d is not found", id));
-    }
-}
